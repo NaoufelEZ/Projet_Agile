38fcd4238fad2ba2c786c48702d53907ba3eddb6
kais-120 <121316071+kais-120@users.noreply.github.com>
2025-01-01 12:41:29 +0100

    modified signup page

--- signup.php
+++ signup.php
@@ -6,7 +6,7 @@ if(isset($_SESSION["login"])){
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
