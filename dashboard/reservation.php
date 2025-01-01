fa60637b15582bbf546e507d9c3d93bc04d3665a
kais-120 <121316071+kais-120@users.noreply.github.com>
2025-01-01 12:26:46 +0100

    change reservation page

--- dashboard/reservation.php
+++ dashboard/reservation.php
@@ -3,10 +3,10 @@ require_once("../connect.php");
 session_start();
 if(isset($_SESSION["login"])){
     $email = $_SESSION["login"];
-    $sql = "SELECT role FROM utilisateur WHERE email = '$email'";
+    $sql = "SELECT role,nom FROM utilisateur WHERE email = '$email'";
     $req = mysqli_query($conn,$sql);
-    $res = mysqli_fetch_row($req)[0];
-    if($res == "Technician" || $res == "Administrateur"){
+    $res = mysqli_fetch_row($req);
+    if($res[0] == "Technician" || $res[0] == "Administrateur"){
         if(isset($_GET["accept"])){
             $type = $_GET["accept"];
             $id = $_GET["id"];
@@ -22,39 +22,42 @@ if(isset($_SESSION["login"])){
             }
             
         }
+        if(isset($_GET["Logout"])){
+            session_unset();
+            session_destroy();
+            header("location:../index.php");
+        }
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-        <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <title>reservation</title>
 </head>
 <body>
 <div class="sidebar">
     <h2>Dashboard</h2>
-    <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Profile</a></li>
-      <li><a href="#">Settings</a></li>
-      <li>
-      <a href="" class="there">Reservation</a>
-        <ul class="sub-menu">
-          <li><a href="./reservation.php">En Attende</a></li>
-          <li><a href="./reservationAccept.php">Accept</a></li>
-          <li><a href="./reservationRefuse.php">Refuse</a></li>
-        </ul>
-      </li>
-      <li><a href="#">Logout</a></li>
-    </ul>
+    <?php
+    require_once("./header.php");
+    ?>
   </div>
   <div class="main-content">
     <div class="top-bar">
-      Welcome to Your Dashboard
+        <div class="avatar">
+            <?php
+        echo "<h3>". $res[1] ."</h3>";
+        ?>
+      <div class='toggle'>
+        <ul>
+          <li><a href='./parametre.php'>Paramétre</a></li>
+          <li><a href='?Logout=true'>Déconnecter</a></li>
+        </ul>
+        </div>
+    </div>
 </div>
 <div class="content">
-
     <table border="1">
         <tr>
         <th>Num</th>
